356d0e878e86a3366960625674074995ad40011b
JustDalo <shishlodan@mail.ru>
2021-12-09 17:15:34 +0300

    socket connection

---
+++ src/main/java/by/tc/task03/client/entity/request/RequestType.java
@@ -0,0 +1,12 @@
+package by.tc.task03.client.entity.request;
+
+import java.io.Serializable;
+
+public enum RequestType implements Serializable {
+    EDIT,
+    CREATE,
+    GET,
+    GET_ALL,
+    REGISTER,
+    LOGIN,
+}
---
+++ src/main/java/by/tc/task03/client/entity/request/ResponseType.java
@@ -0,0 +1,10 @@
+package by.tc.task03.client.entity.request;
+
+import java.io.Serializable;
+
+public enum ResponseType implements Serializable {
+    OK,
+    ERROR,
+    UPDATED,
+    NOTFOUND,
+}
